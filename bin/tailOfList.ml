0eff7b12dedf519c18dd4d1a1e2a6a2d58714ad0
Alex Moraes <alexmoraes251@gmail.com>
2023-08-15 19:13:44 -0300

    Tail of list

--- bin/tailOfList.ml
+++ bin/tailOfList.ml
@@ -1,1 +1,8 @@
-let () = print_endline "Hello, World!"
+let rec last l =
+  match l with [] -> None | h :: [] -> Some h | _ :: t -> last t
+
+let print a =
+  match a with Some s -> print_endline s | None -> print_endline "None"
+
+let a = last [ "a"; "b"; "c"; "d" ]
+let () = print a
