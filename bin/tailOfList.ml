91ad3750dfbbb37028533d7738398df9a98e9aa5
Alex Moraes <alexmoraes251@gmail.com>
2023-08-15 21:36:56 -0300

    Configure code formatter

--- bin/tailOfList.ml
+++ bin/tailOfList.ml
@@ -1,8 +1,13 @@
 let rec last l =
-  match l with [] -> None | h :: [] -> Some h | _ :: t -> last t
+  match l with
+    | [] -> None
+    | h :: [] -> Some h
+    | _ :: t -> last t
 
 let print a =
-  match a with Some s -> print_endline s | None -> print_endline "None"
+  match a with
+    | Some s -> print_endline s
+    | None -> print_endline "None"
 
 let a = last [ "a"; "b"; "c"; "d" ]
 let () = print a
