2a9023695e75e28bc6d517941f2149f27c7d6658
Alex Moraes <alexmoraes251@gmail.com>
2023-08-15 23:06:20 -0300

    Update formating settings

--- bin/nthElementOfList.ml
+++ bin/nthElementOfList.ml
@@ -2,7 +2,11 @@ open Lib
 
 let rec nth n = function
   | [] -> None
-  | h :: t -> if n <= 0 then Some h else nth (n - 1) t
+  | h :: t ->
+      if n <= 0 then
+        Some h
+      else
+        nth (n - 1) t
 
 let () =
   let n = nth 2 [ "a"; "b"; "c"; "d"; "e" ] in
